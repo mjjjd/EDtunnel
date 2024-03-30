32bc925ec831ffed868e7f4db07411fd03cf2d18
mjjjd <151398964+mjjjd@users.noreply.github.com>
2024-03-30 21:48:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '6a6abda7-d75c-404c-b60c-4bb401cce66d';
 
-const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
+const proxyIPs = ['proxyip.fxxk.dedyn.io','proxyip.sg.fxxk.dedyn.io','cdn.anycast.eu.org'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
@@ -14,7 +14,7 @@ let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 // ipv6 proxyIP example remove comment to use
 // let proxyIP = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://dns.google/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is invalid');
@@ -879,7 +879,7 @@ const cn_hostnames = [
 	'www.douban.com',           // Douban - A Chinese social networking service
 	'www.meituan.com',          // Meituan - A Chinese group buying website for local services
 	'www.toutiao.com',          // Toutiao - A news and information content platform
-	'www.ifeng.com',            // iFeng - A popular news website in China
+	'www.i.com',            // i - A popular news website in China
 	'www.autohome.com.cn',      // Autohome - A leading Chinese automobile online platform
 	'www.360.cn',               // 360 - A Chinese internet security company
 	'www.douyin.com',           // Douyin - A Chinese short video platform
@@ -912,7 +912,7 @@ const cn_hostnames = [
 	'www.suning.com',           // Suning - A leading Chinese online retailer
 	'www.zhaopin.com',          // Zhaopin - A Chinese job recruitment platform
 	'www.jianshu.com',          // Jianshu - A Chinese online writing platform
-	'www.mafengwo.cn',          // Mafengwo - A Chinese travel information sharing platform
+	'www.mawo.cn',          // Mawo - A Chinese travel information sharing platform
 	'www.51cto.com',            // 51CTO - A Chinese IT technical community website
 	'www.qidian.com',           // Qidian - A Chinese web novel platform
 	'www.ctrip.com',            // Ctrip - A Chinese travel services provider
